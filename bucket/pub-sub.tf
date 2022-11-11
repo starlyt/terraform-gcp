559e77e5b36dd8852b732a0a80edbb7b4c8bd68f
napoleontabe <tabenatdylan77@gmail.com>
2022-11-11 22:37:37 +0000

    pub sub schema

---
+++ bucket/pub-sub.tf
@@ -0,0 +1,6 @@
+
+resource "google_pubsub_schema" "example" {
+  name = "example"
+  type = "AVRO"
+  definition = "{\n  \"type\" : \"record\",\n  \"name\" : \"Avro\",\n  \"fields\" : [\n    {\n      \"name\" : \"StringField\",\n      \"type\" : \"string\"\n    },\n    {\n      \"name\" : \"IntField\",\n      \"type\" : \"int\"\n    }\n  ]\n}\n"
+}
